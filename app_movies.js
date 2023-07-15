102a913d7aa3b7ab4bf1830ef643ee2c69a1bcd1
Anetadom <anetawdomagala@gmail.com>
2023-07-15 20:26:57 +0200

    filter JS

--- app_movies.js
+++ app_movies.js
@@ -1,23 +1,69 @@
-// onclick genres
+//Filter Data
 
+function collectDataValuesGenre() {
+  const dataValuesGenre = [];
 
-function activeGenres(event) {
-    const button = event.target;
-    
+  const buttons = document.querySelectorAll('#with_genres li button');
+  buttons.forEach(function(button) {
     if (button.classList.contains('active')) {
-      button.classList.remove('active');
-    } else {
-      button.classList.add('active');
+      dataValuesGenre.push(button.parentNode.getAttribute('data-value'));
     }
+  });
+
+  return dataValuesGenre;
+}
+
+//Active Genres
+function activeGenres(event) {
+  const button = event.target;
+
+  if (button.classList.contains('active')) {
+    button.classList.remove('active');
+  } else {
+    button.classList.add('active');
   }
 
-  // API
-
-const ApiKey = "&api_key=57e2a7b6bb030ad38f924e126dc9e94a";
-const ApiBase = "https://api.themoviedb.org/3/discover/movie?";
-const APIMovieRatingDesc = "sort_by=vote_average.desc&vote_count.gte=500";
-const APIMovieRatingDescURL = ApiBase+APIMovieRatingDesc+ApiKey
+  const dataValuesGenre = collectDataValuesGenre().join("%20or%20");
+  API(dataValuesGenre);
+}
 
 
-console.log(APIMovieRatingDescURL)
 
+async function API(dataValuesGenre) {
+  const ApiKey = "&api_key=57e2a7b6bb030ad38f924e126dc9e94a";
+  const ApiBase = "https://api.themoviedb.org/3/discover/movie?";
+  const APIMovieRatingDesc = "sort_by=vote_average.desc&vote_count.gte=1000&with_genres=";
+  const MovieSortBy = document.getElementById("sort-by");
+  const MovieSortByValue = MovieSortBy.value;
+  console.log(MovieSortByValue)
+  const APIMovieRatingDescURL = ApiBase + APIMovieRatingDesc + dataValuesGenre + ApiKey;
+ 
+
+  console.log(APIMovieRatingDescURL);
+  const moviesTrending = await fetch(APIMovieRatingDescURL);
+  const moviesTrendingData = await (moviesTrending.json());
+  console.log(moviesTrending);
+  const moviesTrendingList = document.querySelector(".results__wrapper");
+  moviesTrendingList.innerHTML = moviesTrendingData.results.map( (movie) => moviesPage(movie)).join("");
+}
+
+function moviesPage (movie) {
+  const ApiImageBase = 'https://image.tmdb.org/t/p/w500/';
+  let releaseYear = (new Date(movie.release_date)).getFullYear();
+  return  `<div class="result">
+  <figure class="result__img--wrapper">
+    <img
+      src="${ApiImageBase + movie.poster_path}"
+      alt=""
+      class="result__img"
+    />
+  </figure>
+  <div>
+    <p class="result__title">${movie.title}</p>
+    <p class="result__year">${releaseYear}</p>
+  </div>
+  <figure class="know-more">
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+  </figure>
+</div>`;
+}
